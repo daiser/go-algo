5d60f9e37a29fe7e4ba3e82f1d23c2270a8e217b
Roman Kvitkov <rk@316.su>
2023-10-01 14:21:07 +0500

    Code format

--- olimp/poisk/t1/t1.go
+++ olimp/poisk/t1/t1.go
@@ -3,50 +3,50 @@ package main
 import "fmt"
 
 func getSize(a [][]int) (n, m int) {
-    n = len(a)
-    if n < 1 {
-        panic("empty a")
-    }
+	n = len(a)
+	if n < 1 {
+		panic("empty a")
+	}
 
-    m = len(a[0])
-    for i, an := range a {
-        if len(an) != m {
-            panic(fmt.Errorf("length of a[%d] != %d", i, m))
-        }
-    }
+	m = len(a[0])
+	for i, an := range a {
+		if len(an) != m {
+			panic(fmt.Errorf("length of a[%d] != %d", i, m))
+		}
+	}
 
-    return
+	return
 }
 
 func isFound(a [][]int, indexes []int) bool {
-    found := true
-    for i := range indexes {
-        found = found && (a[0][indexes[0]] == a[i][indexes[i]])
-    }
-    return found
+	found := true
+	for i := range indexes {
+		found = found && (a[0][indexes[0]] == a[i][indexes[i]])
+	}
+	return found
 }
 
 func main() {
-    // http://algolist.manual.ru/olimp/poi_prb.php
-    a := [][]int {
-        {0,0,3},
-        {3,3,3},
-        {1,2,3},
-        {3,4,5},
-        {-1000,3,1000},
-    }
+	// http://algolist.manual.ru/olimp/poi_prb.php
+	a := [][]int{
+		{0, 0, 3},
+		{3, 3, 3},
+		{1, 2, 3},
+		{3, 4, 5},
+		{-1000, 3, 1000},
+	}
 
-    n, m := getSize(a)
-    indexes := make([]int, n)
-    for indexes[0] < m && !isFound(a, indexes) {
-        indexes[0]++
-        for i := 1; i < n; i++ {
-            for ; indexes[i] < m && a[i][indexes[i]] < a[0][indexes[0]]; indexes[i]++ {
-            }
-        }
-    }
-    if indexes[0] < m {
-        println(a[0][indexes[0]])
-        fmt.Printf("%v\n", indexes)
-    }
+	n, m := getSize(a)
+	indexes := make([]int, n)
+	for indexes[0] < m && !isFound(a, indexes) {
+		indexes[0]++
+		for i := 1; i < n; i++ {
+			for ; indexes[i] < m && a[i][indexes[i]] < a[0][indexes[0]]; indexes[i]++ {
+			}
+		}
+	}
+	if indexes[0] < m {
+		println(a[0][indexes[0]])
+		fmt.Printf("%v\n", indexes)
+	}
 }
